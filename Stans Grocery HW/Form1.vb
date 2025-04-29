3d2f25ce25e27a91fa0209a9f70a0e3b65030639
horsjaco117 <jacob_horsley@live.com>
2025-04-29 17:16:07 -0600

    Somewhere of a stopping point

--- Stans Grocery HW/Form1.vb
+++ Stans Grocery HW/Form1.vb
@@ -2,4 +2,17 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub GraphicExamplesForm_activated(sender As Object, e As EventArgs) Handles Me.Activated
+        Static isStartUp As Boolean = True
+        Me.Hide()
+
+
+        If isStartUp Then ' activates at startup
+            SplashScreen.Show() 'See splashform for timer modifications of splashscreen
+            isStartUp = False
+        End If
+
+
+    End Sub
 End Class
---
+++ Stans Grocery HW/SplashScreen.vb
@@ -0,0 +1,17 @@
+﻿Public Class SplashScreen
+    Private Sub Splashscreen_Activated(sender As Object, e As EventArgs) Handles Me.Activated
+        Form1.SendToBack()
+        SplashTimer.Enabled() = True
+        Form1.Hide()
+    End Sub
+
+    Private Sub SplashTimer1_Tick(sender As Object, e As EventArgs) Handles SplashTimer.Tick
+        SplashTimer.Enabled = False
+        Form1.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub SplashScreen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
