7e969a7d3abcb5c6f2089528c98380d9de0997c3
horsjaco117 <jacob_horsley@live.com>
2025-05-08 13:27:04 -0600

    About form added.

---
+++ Stans Grocery HW/AboutForm.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class AboutForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.AboutLabel = New System.Windows.Forms.Label()
+        Me.ReturnButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'AboutLabel
+        '
+        Me.AboutLabel.AutoSize = True
+        Me.AboutLabel.Location = New System.Drawing.Point(25, 20)
+        Me.AboutLabel.Name = "AboutLabel"
+        Me.AboutLabel.Size = New System.Drawing.Size(232, 80)
+        Me.AboutLabel.TabIndex = 0
+        Me.AboutLabel.Text = "Head Developer: Jacob Horsley" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Boss: The Rossitor" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Class: RCET 2265" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'ReturnButton
+        '
+        Me.ReturnButton.Location = New System.Drawing.Point(583, 321)
+        Me.ReturnButton.Name = "ReturnButton"
+        Me.ReturnButton.Size = New System.Drawing.Size(124, 80)
+        Me.ReturnButton.TabIndex = 1
+        Me.ReturnButton.Text = "Return to Stan's Grocery"
+        Me.ReturnButton.UseVisualStyleBackColor = True
+        '
+        'AboutForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ReturnButton)
+        Me.Controls.Add(Me.AboutLabel)
+        Me.Name = "AboutForm"
+        Me.Text = "AboutForm"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents AboutLabel As Label
+    Friend WithEvents ReturnButton As Button
+End Class
