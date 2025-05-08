a53a9b55b191bd8bfd52a3f2c05b4e948150a992
horsjaco117 <jacob_horsley@live.com>
2025-05-08 13:39:36 -0600

    The about form works now, but the splash screen is broken.

--- Stans Grocery HW/AboutForm.vb
+++ Stans Grocery HW/AboutForm.vb
@@ -1,3 +1,6 @@
 ﻿Public Class AboutForm
-
+    Private Sub ReturnButton_Click(sender As Object, e As EventArgs) Handles ReturnButton.Click
+        Me.Hide()
+        StansGrocery.Show()
+    End Sub
 End Class
--- Stans Grocery HW/StansGrocery.vb
+++ Stans Grocery HW/StansGrocery.vb
@@ -1,22 +1,25 @@
 ﻿Public Class StansGrocery
-    Private Sub StansGrocery_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    Private Sub GraphicExamplesForm_activated(sender As Object, e As EventArgs) Handles Me.Activated
-        Static isStartUp As Boolean = True
-        Me.Hide()
 
 
-        If isStartUp Then ' activates at startup
-            SplashScreen.Show() 'See splashform for timer modifications of splashscreen
-            isStartUp = False
-        End If
+    'Private Sub GraphicExamplesForm_activated(sender As Object, e As EventArgs) Handles Me.Activated
+    '    Static isStartUp As Boolean = True
+    '    Me.Hide()
 
 
-    End Sub
+    '    If isStartUp Then ' activates at startup
+    '        SplashScreen.Show() 'See splashform for timer modifications of splashscreen
+    '        isStartUp = False
+    '    End If
+
+
+    'End Sub
 
     Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
         Me.Close()
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        AboutForm.Show()
+        Me.Hide()
     End Sub
 End Class
